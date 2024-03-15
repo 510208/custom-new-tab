95fad433d4147533ffea023e6bdcc849eb56529e
510208 <xux510208@gmail.com>
2024-03-15 15:52:59 +0800

    Update server.py

--- server.py
+++ server.py
@@ -1,12 +1,21 @@
 import socketserver
 import argparse
 import http.server
+import ctypes
 
 # 設定參數
-parser = argparse.ArgumentParser(description="This is a simple HTTP server")
-parser.add_argument("-p", "--port", type=int, default=8000, help="Port number")
-parser.add_argument("-d", "--directory", type=str, default="", help="Directory")
+parser = argparse.ArgumentParser(description="一款非常簡單的本機伺服器啟動腳本，提供給 510208 的 custom-new-tab 進行使用")
+parser.add_argument("-p", "--port", type=int, default=8000, help="本機伺服器端口號")
+parser.add_argument("-d", "--directory", type=str, default="", help="根路徑，不建議修改")
+parser.add_argument("-h", "--hide-cmd", type=bol, default=False, help="是否顯示終端視窗")
 args = parser.parse_args()
+
+# 隱藏視窗
+if hide-cmd:
+    whnd = ctypes.windll.kernel32.GetConsoleWindow()    
+    if whnd != 0:    
+        ctypes.windll.user32.ShowWindow(whnd, 0)    
+        ctypes.windll.kernel32.CloseHandle(whnd)
 
 # 設定伺服器
 Handler = http.server.SimpleHTTPRequestHandler
@@ -14,6 +23,6 @@ Handler.directory = args.directory
 httpd = socketserver.TCPServer(("", args.port), Handler)
 
 # 啟動伺服器
-print("serving at port", args.port)
+print("伺服器啟動在： http://localhost:", args.port)
 
 httpd.serve_forever()
